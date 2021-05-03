d87b4539a305dd343439d52b7fd146accba07155
Villa Mukti Indriyanto <villamuktiindriyanto@gmail.com>
2021-05-04 06:29:21 +0700

    Added a several required class and packages

---
+++ app/src/main/java/team/getherfolg/capstone/ui/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package team.getherfolg.capstone.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import team.getherfolg.capstone.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
