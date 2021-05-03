283bfa1ba3f9883552704de2f2d25f5753241a40
Villa Mukti Indriyanto <villamuktiindriyanto@gmail.com>
2021-05-04 06:25:26 +0700

    Added a several required packages

---
+++ app/src/main/java/team/getherfolg/capstone/ui/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package team.getherfolg.capstone.ui.splashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import team.getherfolg.capstone.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
